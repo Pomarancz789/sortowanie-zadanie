0ad996b8b1dc49dd52bd7212cc3c217ae5b0cf57
Pomarancz <kuba.wtorkiewicz@gmail.com>
2022-01-11 18:50:46 +0100

    Initial Commit

---
+++ sortowanie-zadanie/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sortowanie-zadanie
+//
+//  Created by Kuba Wtorkiewicz on 11/01/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
